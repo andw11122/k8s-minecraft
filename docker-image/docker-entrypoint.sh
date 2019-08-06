b27027071255cc7f153cf4a50a9233905d0ca81a
Hakjun Min <hakjun.min@weather.com>
2019-08-06 17:16:07 +0900

    change properly

--- docker-image/docker-entrypoint.sh
+++ docker-image/docker-entrypoint.sh
@@ -3,8 +3,10 @@ echo "Running entrypoint script."
 
 # Get minecraft jarfile
 echo "Downloading version $VERSION"
-wget -q https://launcher.mojang.com/mc/game/$VERSION/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar -P /home/minecraft/
+#wget -q https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar -P /home/minecraft/
+wget -q https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar -P /home/minecraft/
 
+mv /home/minecraft/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar /home/minecraft/server.jar
 echo "Accepting eula..."
 echo "# Generated via Docker on $(date)" > /minecraft-data/eula.txt
 echo "eula=$EULA" >> /minecraft-data/eula.txt
